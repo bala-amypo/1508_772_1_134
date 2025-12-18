2c6a7cf5681ae8cd0732be3d5d46b5c55d07f8a4
bala-amypo <bala@amypo.in>
2025-12-18 06:35:34 +0000

    User commit

--- src/main/java/com/example/demo/model/RecoveryCurveProfile.java
+++ src/main/java/com/example/demo/model/RecoveryCurveProfile.java
@@ -1,29 +1,63 @@
 package com.example.demo.model;
 
 import jakarta.persistence.*;
-import lombok.Data;
 
 @Entity
-@Table(
-    name = "recovery_curve_profiles",
-    uniqueConstraints = {
-        @UniqueConstraint(columnNames = {"surgeryType", "dayNumber"})
-    }
-)
-@Data
 public class RecoveryCurveProfile {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    @Column(nullable = false)
     private String surgeryType;
 
-    @Column(nullable = false)
     private Integer dayNumber;
 
     private Integer expectedPainLevel;
     private Integer expectedMobilityLevel;
     private Integer expectedFatigueLevel;
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getSurgeryType() {
+        return surgeryType;
+    }
+
+    public void setSurgeryType(String surgeryType) {
+        this.surgeryType = surgeryType;
+    }
+
+    public Integer getDayNumber() {
+        return dayNumber;
+    }
+
+    public void setDayNumber(Integer dayNumber) {
+        this.dayNumber = dayNumber;
+    }
+
+    public Integer getExpectedPainLevel() {
+        return expectedPainLevel;
+    }
+
+    public void setExpectedPainLevel(Integer expectedPainLevel) {
+        this.expectedPainLevel = expectedPainLevel;
+    }
+
+    public Integer getExpectedMobilityLevel() {
+        return expectedMobilityLevel;
+    }
+
+    public void setExpectedMobilityLevel(Integer expectedMobilityLevel) {
+        this.expectedMobilityLevel = expectedMobilityLevel;
+    }
+
+    public Integer getExpectedFatigueLevel() {
+        return expectedFatigueLevel;
+    }
+
+    public void setExpectedFatigueLevel(Integer expectedFatigueLevel) {
+        this.expectedFatigueLevel = expectedFatigueLevel;
+    }
 }
