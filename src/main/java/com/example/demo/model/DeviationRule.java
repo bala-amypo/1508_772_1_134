8303ed6effe72feb89c2345c92e7b25af217637d
bala-amypo <bala@amypo.in>
2025-12-26 08:00:11 +0000

    User commit

--- src/main/java/com/example/demo/model/DeviationRule.java
+++ src/main/java/com/example/demo/model/DeviationRule.java
@@ -1,69 +1,24 @@
 package com.example.demo.model;
 
 import jakarta.persistence.*;
+import lombok.*;
 
 @Entity
-@Table(name = "deviation_rule")
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
 public class DeviationRule {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    private String metricName;
-    private Double minValue;
-    private Double maxValue;
-    private String conditionType;
-    private String alertMessage;
+    @Column(unique = true)
+    private String ruleCode;
 
-    public DeviationRule() {}
-
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public String getMetricName() {
-        return metricName;
-    }
-
-    public void setMetricName(String metricName) {
-        this.metricName = metricName;
-    }
-
-    public Double getMinValue() {
-        return minValue;
-    }
-
-    public void setMinValue(Double minValue) {
-        this.minValue = minValue;
-    }
-
-    public Double getMaxValue() {
-        return maxValue;
-    }
-
-    public void setMaxValue(Double maxValue) {
-        this.maxValue = maxValue;
-    }
-
-    public String getConditionType() {
-        return conditionType;
-    }
-
-    public void setConditionType(String conditionType) {
-        this.conditionType = conditionType;
-    }
-
-    public String getAlertMessage() {
-        return alertMessage;
-    }
-
-    public void setAlertMessage(String alertMessage) {
-        this.alertMessage = alertMessage;
-    }
+    private String parameter;
+    private Integer threshold;
+    private String severity;
+    private Boolean active = true;
 }
-    
