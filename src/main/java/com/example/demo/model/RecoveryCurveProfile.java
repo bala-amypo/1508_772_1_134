c27ddf5a544d72a3f49342dbc64df85cd67f5cec
bala-amypo <bala@amypo.in>
2025-12-26 07:59:45 +0000

    User commit

--- src/main/java/com/example/demo/model/RecoveryCurveProfile.java
+++ src/main/java/com/example/demo/model/RecoveryCurveProfile.java
@@ -1,50 +1,22 @@
 package com.example.demo.model;
 
 import jakarta.persistence.*;
+import lombok.*;
 
 @Entity
-@Table(name = "recovery_curve_profile")
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
 public class RecoveryCurveProfile {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    private String metricName;
-    private Double expectedValue;
-    private Integer timePoint;
-
-    public RecoveryCurveProfile() {}
-
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public String getMetricName() {
-        return metricName;
-    }
-
-    public void setMetricName(String metricName) {
-        this.metricName = metricName;
-    }
-
-    public Double getExpectedValue() {
-        return expectedValue;
-    }
-
-    public void setExpectedValue(Double expectedValue) {
-        this.expectedValue = expectedValue;
-    }
-
-    public Integer getTimePoint() {
-        return timePoint;
-    }
-
-    public void setTimePoint(Integer timePoint) {
-        this.timePoint = timePoint;
-    }
+    private String surgeryType;
+    private Integer dayNumber;
+    private Integer expectedPainLevel;
+    private Integer expectedMobilityLevel;
+    private Integer expectedFatigueLevel;
 }
