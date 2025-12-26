4b856d0227518db48e07cd7cae99898be81a01ab
bala-amypo <bala@amypo.in>
2025-12-26 04:35:13 +0000

    User commit

--- src/main/java/com/example/demo/service/RecoveryCurveProfileService.java
+++ src/main/java/com/example/demo/service/RecoveryCurveProfileService.java
@@ -1,11 +1,16 @@
 package com.example.demo.service;
 
 import com.example.demo.model.RecoveryCurveProfile;
+
 import java.util.List;
 
-public interface RecoveryCurveService {
+public interface RecoveryCurveProfileService {
 
     RecoveryCurveProfile createProfile(RecoveryCurveProfile profile);
 
     List<RecoveryCurveProfile> getAllProfiles();
+
+    RecoveryCurveProfile updateProfile(Long id, RecoveryCurveProfile profile);
+
+    void deleteProfile(Long id);
 }
