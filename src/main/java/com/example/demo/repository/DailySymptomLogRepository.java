141561bc5d4c9720076195deaf3e4fc446b754aa
bala-amypo <bala@amypo.in>
2025-12-26 08:01:39 +0000

    User commit

--- src/main/java/com/example/demo/repository/DailySymptomLogRepository.java
+++ src/main/java/com/example/demo/repository/DailySymptomLogRepository.java
@@ -1,14 +1,14 @@
+// DailySymptomLogRepository.java
 package com.example.demo.repository;
 
 import com.example.demo.model.DailySymptomLog;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.time.LocalDate;
-import java.util.List;
+import java.util.*;
 
 public interface DailySymptomLogRepository extends JpaRepository<DailySymptomLog, Long> {
 
-    List<DailySymptomLog> findByLogDate(LocalDate logDate);
-
-    List<DailySymptomLog> findBySeverity(String severity);
+    List<DailySymptomLog> findByPatientId(Long patientId);
+    Optional<DailySymptomLog> findByPatientIdAndLogDate(Long patientId, LocalDate logDate);
 }
