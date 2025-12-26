9dfb1f6983396395c64bb039f675d0b44890ebc8
bala-amypo <bala@amypo.in>
2025-12-26 08:14:29 +0000

    User commit

--- src/main/java/com/example/demo/config/SwaggerConfig.java
+++ src/main/java/com/example/demo/config/SwaggerConfig.java
@@ -1,9 +1,11 @@
 package com.example.demo.config;
 
 import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
 import io.swagger.v3.oas.models.servers.Server;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+
 import java.util.List;
 
 @Configuration
@@ -11,10 +13,16 @@ public class SwaggerConfig {
 
     @Bean
     public OpenAPI customOpenAPI() {
+
+        Server server = new Server();
+        server.setUrl("https://9032.pro604cr.amypo.ai/");
+        server.setDescription("Production Server");
+
         return new OpenAPI()
-                // You need to change the port as per your server
-                .servers(List.of(
-                        new Server().url("https://9032.pro604cr.amypo.ai/")
-                ));
-        }
+                .info(new Info()
+                        .title("Post Surgery Recovery Tracker API")
+                        .version("1.0")
+                        .description("API documentation for Post Surgery Recovery System"))
+                .servers(List.of(server));
+    }
 }
