128ba598449133391dbbc38aece943294192b415
bala-amypo <bala@amypo.in>
2025-12-26 05:38:07 +0000

    User commit

--- src/main/java/com/example/demo/controller/RecoveryCurveProfileController.java
+++ src/main/java/com/example/demo/controller/RecoveryCurveProfileController.java
@@ -1,8 +1,7 @@
 package com.example.demo.controller;
 
-import com.example.demo.model.RecoveryCurveProfile;
-import com.example.demo.service.RecoveryCurveProfileService;
-
+import com.example.demo.model.RecoveryCurve;
+import com.example.demo.service.RecoveryCurveService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
