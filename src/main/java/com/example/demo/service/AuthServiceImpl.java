453e3f8ff968324eb80cb5df6da05ccf085235e9
bala-amypo <bala@amypo.in>
2025-12-26 06:16:07 +0000

    User commit

---
+++ src/main/java/com/example/demo/service/AuthServiceImpl.java
@@ -0,0 +1,34 @@
+package com.example.demo.service.impl;
+
+import com.example.demo.model.AppUser;
+import com.example.demo.repository.AppUserRepository;
+import com.example.demo.service.AuthService;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+
+@Service
+public class AuthServiceImpl implements AuthService {
+
+    private final AppUserRepository appUserRepository;
+    private final PasswordEncoder passwordEncoder;
+
+    public AuthServiceImpl(AppUserRepository appUserRepository,
+                           PasswordEncoder passwordEncoder) {
+        this.appUserRepository = appUserRepository;
+        this.passwordEncoder = passwordEncoder;
+    }
+
+    @Override
+    public AppUser register(AppUser user) {
+
+        // Encode password
+        user.setPassword(passwordEncoder.encode(user.getPassword()));
+
+        // Default role if not provided
+        if (user.getRole() == null) {
+            user.setRole("PATIENT");
+        }
+
+        return appUserRepository.save(user);
+    }
+}
