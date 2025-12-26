ef2ab11f5df99da6da246a8e56e5b87ca5257446
bala-amypo <bala@amypo.in>
2025-12-26 04:26:17 +0000

    User commit

--- src/main/java/com/example/demo/repository/ClinicalAlertRepository.java
+++ src/main/java/com/example/demo/repository/ClinicalAlertRepository.java
@@ -2,7 +2,9 @@ package com.example.demo.repository;
 
 import com.example.demo.model.ClinicalAlert;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
-public interface ClinicalAlertRecordRepository
+@Repository
+public interface ClinicalAlertRepository
         extends JpaRepository<ClinicalAlert, Long> {
 }
